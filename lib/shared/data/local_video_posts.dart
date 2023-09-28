f7ef95fc26688501c20b07bacfcab5822650c0bd
Javier García <a2027garciaj@gmail.com>
2023-09-28 13:40:28 +0200

    App par ver videos estilo TikTok

---
+++ lib/shared/data/local_video_posts.dart
@@ -0,0 +1,52 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Subiendo escaleras automáticas',
+    'videoUrl': 'assets/videos/1.mp4',
+    'likes': 23230,
+    'views': 1523,
+  },
+  {
+    'name': 'Planta apreciada por peatones',
+    'videoUrl': 'assets/videos/2.mp4',
+    'likes': 24230,
+    'views': 1343,
+  },
+  {
+    'name': 'Que borroso veo todo!',
+    'videoUrl': 'assets/videos/3.mp4',
+    'likes': 21564320,
+    'views': 123563,
+  },
+  {
+    'name': '¿Esto es trigo? que interesante',
+    'videoUrl': 'assets/videos/4.mp4',
+    'likes': 320,
+    'views': 2300,
+  },
+  {
+    'name': 'El COVID no me afecta',
+    'videoUrl': 'assets/videos/5.mp4',
+    'likes': 3230,
+    'views': 31030,
+  },
+  {
+    'name': 'De aquí salen las pipas 😯',
+    'videoUrl': 'assets/videos/6.mp4',
+    'likes': 10,
+    'views': 330,
+  },
+  {
+    'name': 'Huelva es la mejor ciudad 🧐',
+    'videoUrl': 'assets/videos/7.mp4',
+    'likes': 1320,
+    'views': 33032,
+  },
+  {
+    'name': '¿Ya llegamos a la India?... umm si',
+    'videoUrl': 'assets/videos/8.mp4',
+    'likes': 342,
+    'views': 3332,
+  },
+
+
+];
